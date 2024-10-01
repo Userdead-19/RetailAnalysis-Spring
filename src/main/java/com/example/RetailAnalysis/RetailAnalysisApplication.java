6cbcf3574cc75a39ecedda86598a55803733eed7
Abinav <abinav10102@gmail.com>
2024-10-01 10:02:17 +0530

    Add application.properties file and .gitignore
    - Added application.properties file with spring.application.name set to "RetailAnalysis".
    - Added .gitignore file with standard exclusions for IDEs and build artifacts.

---
+++ src/main/java/com/example/RetailAnalysis/RetailAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetailAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAnalysisApplication.class, args);
+	}
+
+}
